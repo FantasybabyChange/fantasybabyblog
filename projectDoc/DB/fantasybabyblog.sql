3da91e694ebf67a4bd21e10aa2bb9f58759dcacc
FantasyBaby <625353861@qq.com>
2015-11-15 22:09:16 +0800

    complete the db design and add the sql

---
+++ projectDoc/DB/fantasybabyblog.sql
@@ -0,0 +1,159 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2015-11-15 21:25:18                          */
+/*author: FantasyBaby*/
+/*==============================================================*/
+
+ DROP DATABASE IF EXISTS `fantasybabyblog`;
+ CREATE DATABASE `fantasybabyblog` 
+ DEFAULT CHARACTER SET utf8 
+ COLLATE utf8_general_ci;
+ USE fantasybabyblog;
+
+DROP TABLE IF EXISTS _article;
+
+DROP TABLE IF EXISTS _category;
+
+DROP TABLE IF EXISTS _privilege;
+
+DROP TABLE IF EXISTS _tag;
+
+DROP TABLE IF EXISTS article_tag;
+
+DROP TABLE IF EXISTS blog_user;
+
+DROP TABLE IF EXISTS category_privilege;
+
+DROP TABLE IF EXISTS category_type;
+
+DROP TABLE IF EXISTS user_detail;
+
+/*==============================================================*/
+/* Table: _article                                              */
+/*==============================================================*/
+CREATE TABLE _article
+(
+   _uuid                CHAR(32),
+   _id                  SMALLINT NOT NULL,
+   title                VARCHAR(100),
+   abstract             VARCHAR(300),
+   author               VARCHAR(50),
+   publish_user         SMALLINT,
+   content              TEXT,
+   STATUS               SMALLINT,
+   category             SMALLINT,
+   create_date          DATETIME,
+   modify_date          DATETIME,
+   publish_date         DATETIME,
+   PRIMARY KEY (_id)
+);
+
+/*==============================================================*/
+/* Table: _category                                             */
+/*==============================================================*/
+CREATE TABLE _category
+(
+   _uuid                CHAR(32),
+   _id                  SMALLINT NOT NULL,
+   categorname          VARCHAR(30),
+   categorytype         SMALLINT,
+   create_date          DATETIME,
+   PRIMARY KEY (_id)
+);
+
+/*==============================================================*/
+/* Table: _privilege                                            */
+/*==============================================================*/
+CREATE TABLE _privilege
+(
+   _uuid                CHAR(32) NOT NULL,
+   pid                  SMALLINT NOT NULL,
+   pname                VARCHAR(30),
+   ppath                VARCHAR(300),
+   PRIMARY KEY (pid)
+)ENGINE = INNODB;
+
+/*==============================================================*/
+/* Table: _tag                                                  */
+/*==============================================================*/
+CREATE TABLE _tag
+(
+   _uuid                CHAR(32),
+   _id                  SMALLINT NOT NULL,
+   tagName              VARCHAR(30),
+   PRIMARY KEY (_id)
+)ENGINE = INNODB;
+
+/*==============================================================*/
+/* Table: article_tag                                           */
+/*==============================================================*/
+CREATE TABLE article_tag
+(
+   _uuid                CHAR(32) NOT NULL,
+   tagid                SMALLINT,
+   userid               SMALLINT,
+   PRIMARY KEY (_uuid)
+)ENGINE = INNODB;
+
+/*==============================================================*/
+/* Table: blog_user                                             */
+/*==============================================================*/
+CREATE TABLE blog_user
+(
+   _uuid                CHAR(32) NOT NULL,
+   _id                  SMALLINT NOT NULL,
+   username             VARCHAR(20),
+   PASSWORD             VARCHAR(300),
+   categoryid           SMALLINT,
+   create_date          DATETIME,
+   modify_date          DATETIME,
+   PRIMARY KEY (_id)
+)ENGINE = INNODB;
+
+ALTER TABLE blog_user COMMENT 'user in blog.they have their own category.like admin or view';
+
+/*==============================================================*/
+/* Table: category_privilege                                    */
+/*==============================================================*/
+CREATE TABLE category_privilege
+(
+   _uuid                CHAR(32) NOT NULL,
+   privilegeid          SMALLINT,
+   categoryid           SMALLINT,
+   PRIMARY KEY (_uuid)
+)ENGINE = INNODB;
+
+/*==============================================================*/
+/* Table: category_type                                         */
+/*==============================================================*/
+CREATE TABLE category_type
+(
+   _uuid                CHAR(32),
+   _id                  SMALLINT NOT NULL,
+   categorname          VARCHAR(30),
+   categorytype         SMALLINT,
+   create_date          DATETIME,
+   PRIMARY KEY (_id)
+)ENGINE = INNODB;
+
+/*==============================================================*/
+/* Table: user_detail                                           */
+/*==============================================================*/
+CREATE TABLE user_detail
+(
+   _uuid                CHAR(32) NOT NULL,
+   userid               SMALLINT,
+   NAME                 VARCHAR(20),
+   sex                  BOOL,
+   phone                VARCHAR(12),
+   email                VARCHAR(30),
+   address              VARCHAR(50),
+   hometown             VARCHAR(50),
+   profession           VARCHAR(38),
+   nickname             VARCHAR(20),
+   image                VARCHAR(40),
+   PRIMARY KEY (_uuid)
+)ENGINE = INNODB;
+
+ALTER TABLE user_detail COMMENT 'use to recording user''s information';
+
